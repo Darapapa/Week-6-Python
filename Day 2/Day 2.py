6a0885e6ff5ee50f5dd00b8a063acb2bf12998f9
Darapapa <wilhelmdani@freemail.hu>
2022-04-25 20:26:10 +0300

    Day 2 Mandatory Exercises (1)

---
+++ Day 2/Day 2.py
@@ -0,0 +1,57 @@
+# # Day 2 Exercise 1:
+# hello = "Hello World"
+# print(hello + hello +hello +hello)
+
+# Exercise 2:
+# x = pow(99, 3) * 8
+# print(x)
+
+# Exercise 3:
+# Predict the output of the following code snippets:
+# 5 < 3  #false
+# 3 == 3 = true
+# 3 == "3" = false
+# "3" > 3  # TypeError not ">" not supported between str and int
+#"Hello" == "hello" = false
+
+# Exercise 4:
+# Create a variable called computer_brand which value is the brand name of your computer.
+# Using the computer_brand variable print a sentence that states the following: "I have a <computer_brand> computer".
+# computer_brand = "Lenovo"
+# print("i have a "+ computer_brand +" computer")
+
+# Exercise 5:
+# name = "Daniel"
+# age = 30
+# shoe_size = 42
+# info = "My name is %s" % (name) + " I am %s" % (age) + "My shoe size is %s" % (shoe_size)
+# print(info)
+
+# Exercise 6:
+# a = 8
+# b = 6
+# if a > b:
+#       print("Hello World")
+
+# Exercise 7:
+# number = int(input("please enter a number between 0-100: "))
+# mod = number % 2
+# if mod > 0 :
+#       print("this is a odd number")
+# else:
+#       print("this is an even number")
+
+# Exercise 8:
+# user_name = (input("Please enter your name:"))
+# if user_name in ["Daniel", "daniel", "dani", "Dani"]:
+#       print("We have a same name.wow")
+# else:
+#       print("We almost have the same name.Only a few letters difference")
+
+# Exercise 9:
+# user_height = int(input("Please tell us your height in inch:"))
+# user_h_cm = user_height * 2.54
+# if user_h_cm > 145:
+#       print("You are tall enough to ride.")
+# else:
+#       print("You are not tall enough for the ride")
